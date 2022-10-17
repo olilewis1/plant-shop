7cdf13f72b025ec6864adc3dbecd2905753e1d39
olilewis1 <olilewis1@hotmail.co.uk>
2022-10-17 20:51:30 +0100

    added sopme sass

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,20 +1,22 @@
 import React from 'react'
 import mainImageHome from '../assets/Ventura_files/static1.squarespace.jpg'
+import closeUpCoffeeImage from '../assets/Ventura_files/2015-08-11_home-goods-9-1143-detail-FINAL.jpg'
 const Home = () => {
   return (
-    <div>
-      <div> 
-        <img src={mainImageHome} alt='bo' />
+    <div >
+      <div className=''> 
+        <img src={mainImageHome} alt='bo' className="img-fluid h-50"/>
       </div>
-      <div> 
-        <p>Brightening homes with unique, durable products.</p>
+      <div className='container'> 
+      <div class="row m-5 p-5 d-flex justify-content-center"> 
+        <h1 className="text-center w-50 ">Brightening homes with unique, durable products.</h1>
       </div>
-      <div> 
-        <div> 
-          <img src='hi.jpg' alt='hi' />
+      <div class="container d-flex justify-content-evenly w-75"> 
+        <div className="w-50"> 
+          <img src={closeUpCoffeeImage}  alt='hi' className='img-fluid'/>
         </div>
-        <div> 
-          <div> 
+        <div className='d-flex flex-column justify-content-evenly '> 
+          <div className="mt-4"> 
             <p>Featured</p>
           </div>
           <div> 
@@ -26,10 +28,23 @@ const Home = () => {
           <div>
             <p>$50.00</p>
           </div>
-          <div>
+          <div className='mb-3'>
             <button>Shop All</button>
           </div>
         </div>
+      </div>
+      <div> 
+        <div>
+          <img src='corecollection.jpg' alt='hi' />
+          <p>Explore</p>
+          <p>The Core Collection</p>
+        </div>
+        <div> 
+          <img src='setsandpairs.jpg' alt='hi'/>
+          <p>Start Fresh</p>
+          <p>Sets and Pairs</p>
+        </div>
+      </div>
       </div>
     </div>
   )
