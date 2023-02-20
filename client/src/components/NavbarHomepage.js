b739fe5fd439836c16f9e91c919b831419e47650
olilewis1 <olilewis1@hotmail.co.uk>
2023-02-20 17:28:41 +0000

    added redux to work for homepage

--- client/src/components/NavbarHomepage.js
+++ client/src/components/NavbarHomepage.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { counterActions } from '../Store/index'
 import { useDispatch } from 'react-redux';
 import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom'
 //import mainImageHome from '../assets/Ventura_files/static1.squarespace.jpg'
 const NavbarHomepage = () => {
   const dispatch = useDispatch();
@@ -19,7 +20,9 @@ const NavbarHomepage = () => {
 
   };
  const counterState =  useSelector((state) => state.counter) 
- console.log('counter', counterState)
+ 
+
+
   return (
     <div> 
       {counterState.counter ?     <div className='nav-home text-light'>
@@ -30,40 +33,42 @@ const NavbarHomepage = () => {
      
      
       <div className='ms-5' > 
-        <button name='two' onClick={incrementHandler}>Love-Mambo</button>
+        <Link to='/' name='two' onClick={incrementHandler} className="btn text-light btn-outline-* fs-1 ">Love-Mambo</Link>
       </div>
-      <div className='d-flex flex-row me-5 justify-content-center mt-3 mw-100'> 
-        <button onClick={incrementHandler} className='me-3' name='one'>Shop</button>
-        <h5 onClick={incrementHandler} className='me-3' name='one' >About</h5>
-        <h5 onClick={incrementHandler} className='me-3' name='one'>Contact</h5>
-        <h5 onClick={incrementHandler} className='me-3' name='one'>Insta</h5>
-        <h5 onClick={incrementHandler} className='me-3' name='one'>Face</h5>
-        <button onClick={incrementHandler} className='me-3' name='three'>Basket</button>
+      <div className='d-flex flex-row me-5 justify-content-center 
+      mw-100'> 
+        <Link to='/shop' onClick={incrementHandler} className='me-3 btn text-light btn-outline-* fs-4 ' name='one'>Shop</Link>
+        <Link to='/about' onClick={incrementHandler} className='me-3 btn text-light btn-outline-* fs-4 ' name='one' >About</Link>
+        <Link to='/contact' onClick={incrementHandler} className='me-3 btn text-light btn-outline-* fs-4 ' name='one'>Contact</Link>
+        <a  href='https://www.bbc.co.uk'  onClick={incrementHandler} className='me-3 btn text-light btn-outline-* fs-4 ' name='one'>Insta</a>
+        <a  href='https://www.bbc.co.uk'  onClick={incrementHandler} className='me-3 btn text-light btn-outline-* fs-4 ' name='one'>Face</a>
+        <Link to='/basket' onClick={incrementHandler} className='me-3 btn text-light btn-outline-* fs-4 ' name='one'>Basket</Link>
       </div>
     </div>
     </div> : 
     
     
-    <div className='nav-home-No-Image text-light'>
-    {/* <div className=''> 
-      <img src={mainImageHome} alt='bo' className="img-fluid h-50"/>
-    </div>  */}
-      <div className='d-flex flex-row justify-content-between navbar navbar-expand-sm  navbar-expand-sm  navbar-light '>
-       
-       
-        <div className='ms-5' > 
-          <button name='two' onClick={incrementHandler}>Love-Mambo</button>
-        </div>
-        <div className='d-flex flex-row me-5 justify-content-center mt-3 mw-100'> 
-          <button onClick={incrementHandler} className='me-3' name='one'>Shop</button>
-          <h5 onClick={incrementHandler} className='me-3' name='one' >About</h5>
-          <h5 onClick={incrementHandler} className='me-3' name='one'>Contact</h5>
-          <h5 onClick={incrementHandler} className='me-3' name='one'>Insta</h5>
-          <h5 onClick={incrementHandler} className='me-3' name='one'>Face</h5>
-          <button onClick={incrementHandler} className='me-3' name='three'>Basket</button>
-        </div>
+<div className='nav-home-No-Image text-dark'>
+  {/* <div className=''> 
+    <img src={mainImageHome} alt='bo' className="img-fluid h-50"/>
+  </div>  */}
+    <div className='d-flex flex-row justify-content-between navbar navbar-expand-sm  navbar-expand-sm  navbar-light '>
+     
+     
+      <div className='ms-5' > 
+        <Link to='/' name='two' onClick={incrementHandler} className="btn text-dark btn-outline-* fs-1 ">Love-Mambo</Link>
       </div>
-      </div> }
+      <div className='d-flex flex-row me-5 justify-content-center 
+      mw-100'> 
+        <Link to='/shop' onClick={incrementHandler} className='me-3 btn text-dark btn-outline-* fs-4 ' name='one'>Shop</Link>
+        <Link to='/about' onClick={incrementHandler} className='me-3 btn text-dark btn-outline-* fs-4 ' name='one' >About</Link>
+        <Link to='/contact' onClick={incrementHandler} className='me-3 btn text-dark btn-outline-* fs-4 ' name='one'>Contact</Link>
+        <a  href='https://www.bbc.co.uk'  onClick={incrementHandler} className='me-3 btn text-dark btn-outline-* fs-4 ' name='one'>Insta</a>
+        <a  href='https://www.bbc.co.uk'  onClick={incrementHandler} className='me-3 btn text-dark btn-outline-* fs-4 ' name='one'>Face</a>
+        <Link to='/basket' onClick={incrementHandler} className='me-3 btn text-dark btn-outline-* fs-4 ' name='one'>Basket</Link>
+      </div>
+    </div>
+    </div>}
 
     </div>
   )
