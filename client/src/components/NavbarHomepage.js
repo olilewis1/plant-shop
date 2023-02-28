0bbc83fba1caed70dfcf408871940d2e228f4931
olilewis1 <olilewis1@hotmail.co.uk>
2023-02-28 15:09:17 +0000

    added location for nav

--- client/src/components/NavbarHomepage.js
+++ client/src/components/NavbarHomepage.js
@@ -3,11 +3,20 @@ import "bootstrap/js/src/collapse.js";
 import { counterActions } from '../Store/index'
 import { useDispatch } from 'react-redux';
 import { useSelector } from 'react-redux';
+import Nav from 'react-bootstrap/Nav'
 import { Link, useLocation } from 'react-router-dom'
 //import mainImageHome from '../assets/Ventura_files/static1.squarespace.jpg'
 const NavbarHomepage = () => {
   const dispatch = useDispatch();
   const location = useLocation()
+
+console.log(location)
+   if (location.pathname === '/') {
+    dispatch(counterActions.decrement())
+    console.log('hissss')
+  }
+ const [toggleNavClass, setToggleNavClass ] = useState(true)
+ 
   const incrementHandler = (event) => {
     console.log(event.target.name);
     if (event.target.name === "one") {
@@ -19,24 +28,19 @@ const NavbarHomepage = () => {
     if (event.target.name === "three") {
       dispatch(counterActions.toggleCounter())
     }
-
+    const falseTog = false 
+    const trueTog = true
+    if (toggleNavClass === true) { 
+      console.log('hi', toggleNavClass)
+      setToggleNavClass(falseTog)
+    }
+    if (toggleNavClass === false) { 
+      setToggleNavClass(trueTog)
+    }
   };
  const counterState =  useSelector((state) => state.counter) 
- 
- const [toggleNavClass, setToggleNavClass ] = useState(true)
-const handleNavToggle = () =>  { 
-console.log('omne', toggleNavClass)
-const falseTog = false 
-const trueTog = true
-if (toggleNavClass === true) { 
-  console.log('hi', toggleNavClass)
-  setToggleNavClass(falseTog)
-}
-if (toggleNavClass === false) { 
-  setToggleNavClass(trueTog)
-}
 
-}
+
 
 
 
@@ -46,17 +50,18 @@ if (toggleNavClass === false) {
       
       <div className='nav-home text-light'>
         
-      <nav class="navbar navbar-expand-lg navbar-light">
+        
+      <Nav class="navbar navbar-expand-lg navbar-light">
   <div class="container-fluid "> <Link to='/' name='two' onClick={incrementHandler} className="btn text-light btn-outline-* fs-1 navbar-brand">Love-Mambo</Link>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     
-    <div class=' collapse navbar-collapse justify-content-md-end ' id="navbarTogglerDemo02">
+    <div class={ `${!toggleNavClass ? ' ' : 'Show'} collapse navbar-collapse justify-content-md-end`} id="navbarTogglerDemo02">
 
       <div class="navbar-nav d-sm-flex align-items-start" >
 
-      <Link to={'/shop'} onClick={incrementHandler} className='me-3 btn text-light btn-outline-* fs-4 nav-item' data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" name='one'  >Shop</Link>
+      <Link to={'/shop'} onClick={incrementHandler} className='me-3 btn text-light btn-outline-* fs-4 nav-item' name='one'  >Shop</Link>
         <Link to='/about' onClick={incrementHandler} className='me-3 btn text-light btn-outline-* fs-4 nav-item' name='one' >About</Link>
         <Link to='/contact' onClick={incrementHandler} className='me-3 btn text-light btn-outline-* fs-4 nav-item' name='one'>Contact</Link>
         <a  href='https://www.bbc.co.uk'  onClick={incrementHandler} className='me-3 btn text-light btn-outline-* fs-4 nav-item' name='one'>Insta</a>
@@ -65,7 +70,7 @@ if (toggleNavClass === false) {
       </div>
     </div>
   </div>
-</nav>
+</Nav>
         
   {/* <div className=''> 
     <img src={mainImageHome} alt='bo' className="img-fluid h-50"/>
@@ -79,11 +84,11 @@ if (toggleNavClass === false) {
       <span class="navbar-toggler-icon"></span>
     </button>
     
-    <div class=' collapse navbar-collapse justify-content-md-end ' id="navbarTogglerDemo02">
+    <div class={ `${!toggleNavClass ? ' ' : 'Show'} collapse navbar-collapse justify-content-md-end`} id="navbarTogglerDemo02">
 
-      <div class="navbar-nav d-sm-flex align-items-start" >
+      <div class="navbar-nav d-sm-flex align-items-start"  >
 
-      <Nav.Link to='/shop' onClick={incrementHandler} className='me-3 btn  text-dark btn-outline-* fs-4 nav-item' data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" name='one'  >Shop</Nav.Link>
+      <Link to='/shop' onClick={incrementHandler} className='me-3 btn  text-dark btn-outline-* fs-4 nav-item' name='one'  >Shop</Link>
         <Link to='/about' onClick={incrementHandler} className='me-3 btn  text-dark btn-outline-* fs-4 nav-item' name='one' >About</Link>
         <Link to='/contact' onClick={incrementHandler} className='me-3 btn  text-dark btn-outline-* fs-4 nav-item' name='one'>Contact</Link>
         <a  href='https://www.bbc.co.uk'  onClick={incrementHandler} className='me-3 btn  text-dark btn-outline-* fs-4 nav-item' name='one'>Insta</a>
