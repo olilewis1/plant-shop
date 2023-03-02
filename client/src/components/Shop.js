82d62ff62127d9eb4364419cc15c6a431565cd00
olilewis1 <olilewis1@hotmail.co.uk>
2023-03-02 21:16:18 +0000

    added location for nav

--- client/src/components/Shop.js
+++ client/src/components/Shop.js
@@ -2,7 +2,15 @@ import React from 'react'
 
 const Shop = () => {
   return (
-    <div>Shop</div>
+    <div>
+      
+      <div> 
+        <p className='fs-1 text'> Shop</p>
+
+      </div>
+      <div className='container'>      <hr className=''></hr> </div>
+
+    </div>
   )
 }
 
