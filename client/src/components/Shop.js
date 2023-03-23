3137f1bfcc8014c4ecd7dc49817ac319b900f1eb
olilewis1 <olilewis1@hotmail.co.uk>
2023-03-23 16:14:58 +0000

    structure of shop added

--- client/src/components/Shop.js
+++ client/src/components/Shop.js
@@ -1,11 +1,11 @@
 import React from 'react'
-
+import shopImage from '../assets/shopimage.jpg'
 const Shop = () => {
   return (
     <div>
       
       <div> 
-        <p className='fs-1 text'> Shop</p>
+        <p className='fs-1 text d-flex justify-content-center'> Shop</p>
 
       </div>
       <div className='container'>     
@@ -13,6 +13,69 @@ const Shop = () => {
       
       </div>
 
+  <div className='container'>
+<div className='row pt-5'> 
+<div class="card col border-0" >
+  <img class="card-img-top" src={shopImage} alt="Card image cap" />
+  <div class="pt-4">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">$50.00</p>
+    <button  class="btn btn-dark">Buy Now</button>
+
+</div>
+</div>
+      <div class="card col border-0" >
+  <img class="card-img-top" src={shopImage} alt="Card image cap" />
+  <div class="pt-4">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">$50.00</p>
+    <button  class="btn btn-dark">Buy Now</button>
+
+</div>
+</div>
+
+<div class="card col border-0" >
+  <img class="card-img-top" src={shopImage} alt="Card image cap" />
+  <div class="pt-4">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">$50.00</p>
+    <button  class="btn btn-dark">Buy Now</button>
+
+</div>
+</div>
+</div>
+<div className='row pt-5'> 
+<div class="card col border-0" >
+  <img class="card-img-top" src={shopImage} alt="Card image cap" />
+  <div class="pt-4">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">$50.00</p>
+    <button  class="btn btn-dark">Buy Now</button>
+
+</div>
+</div>
+      <div class="card col border-0" >
+  <img class="card-img-top" src={shopImage} alt="Card image cap" />
+  <div class="pt-4">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">$50.00</p>
+    <button  class="btn btn-dark">Buy Now</button>
+
+</div>
+</div>
+
+<div class="card col border-0" >
+  <img class="card-img-top" src={shopImage} alt="Card image cap" />
+  <div class="pt-4">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">$50.00</p>
+    <button  class="btn btn-dark">Buy Now</button>
+
+</div>
+</div>
+</div>
+
+      </div>
     </div>
   )
 }
